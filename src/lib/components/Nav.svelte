06d5085357872a184cb447f457fe75fe22bc61e0
01dog <bellwitch@pm.me>
2022-12-27 20:22:39 -0500

    added contact page

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -6,6 +6,7 @@
 		<div class="text-base-content items-stretch hidden lg:flex">
 			<a href="/" class="btn btn-ghost btn-sm rounded-btn">home</a>
 			<a href="/howto" class="btn btn-ghost btn-sm rounded-btn">how to</a>
+			<a href="/contact" class="btn btn-ghost btn-sm rounded-btn">contact me</a>
 			<a href="/privacy" class="btn btn-ghost btn-sm rounded-btn">privacy</a>
 			<!-- TODO: we should add an extra button here for authenticated users to browse to the actual tool -->
 		</div>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-col justify-center">
+	<h1 class="text-5xl font-bold text-center">contact me</h1>
+	<h3 class="m-4 text-2xl text-center">this site was made by wes @ store 09204</h3>
+	<p class="text-lg">
+		for questions, comments, or anything else, please email me at: <span class="bg-neutral"
+			>rfn+wmercer [at] pm.me</span
+		>.
+	</p>
+</div>
